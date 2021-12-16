be53c8c4c0be5a40236dce3d2b1833c332f35179
jinkun <jin2jin700@gmail.com>
2021-12-17 01:06:37 +0900

    jinkun kyounobunn

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,35 +13,27 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  # 全部の形式をjpgにすることで、全ての形式に対応できる。
+  process :convert => 'jpg'
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-  process resize_to_fit: [400, 400]
-  # Create different versions of your uploaded files:
-  # version :thumb do
+  process resize_to_fill: [240, 376, "Center"]
 
-  # end
+  version :thumb250 do
+    process resize_to_fit: [250, 250]
+  end
 
-  # Add an allowlist of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+   # jpg,jpeg,gif,pngしか受け付けない
+   def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   root 'pages#shiori'
   get '/shiori' , to: 'pages#shiori'
   get '/index' , to: 'books#index'
-  get '/books/new', to: 'books#new'
-  get '/books/:id' , to: 'books#show'
-  get '/books/:id/edit' , to: 'books#edit' 
-  
-  post '/books', to: 'books#create'
-  patch '/books/:id/' , to:  'books#update'
-  delete '/books/:id/' , to: 'books#delete'
+
   resources :books
 end
